4175551e4896698458bee6f67b1350064b82a35c
cooms <thcoomer@hotmail.com>
2017-09-13 22:37:39 +1200

    added arrows between images

--- client/components/PlaydateSection.jsx
+++ client/components/PlaydateSection.jsx
@@ -9,6 +9,9 @@ export default React.createClass({
           <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
           <div className="playdateImageWrapper">
             <img src="/images/bella.jpg" className="playdateImage left"/>
+            <img src="/images/arrow.fw.png" className="arrowImg reversedVert bottom"/>
+            <img src="/images/arrow.fw.png" className="arrowImg"/>
+
             <img src="/images/lily.jpg" className="playdateImage right"/>
           </div>
         </div>
